c1398803e69e459da30099b1f15450fb112c4a65
Mais <59611735+itsmais@users.noreply.github.com>
2020-08-19 01:53:42 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,7 @@ function sleepNow(){
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
-
-    console.log(hours);
-    console.log(minutes);
+    
     // 5 cycles
     date.setMinutes(minutes + 450)
     let idealHours = 0;
@@ -14,6 +12,5 @@ function sleepNow(){
     sleepResult.innerHTML="</br>It's "+hours+":"+minutes+" now."
     + " You should <span class=\"time-result\">set your alarm at "+date.getHours()+":"+date.getMinutes()+"</span>"
     +"</br>to complete five 90-minute sleep cycles.";
-    // 6 cycles
-    
+
 }
