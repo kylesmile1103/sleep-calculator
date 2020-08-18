f3ea6c8a173b946b0d7272af0aa207061d40bf3e
Mais <59611735+itsmais@users.noreply.github.com>
2020-08-18 16:39:44 +0300

    Added favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 let sleepResult = document.getElementById("sleep-result");
 
 function sleepNow(){
-    console.log("sleep now");
-    sleepResult.innerHTML = "sleep now you asshole."
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    console.log(hours);
+    console.log(minutes);
+    // 5 cycles
+    date.setMinutes(minutes + 450)
+    let idealHours = 0;
+    let idealMinutes = 0;
+    sleepResult.innerHTML="</br>It's "+hours+":"+minutes+" now."
+    + " You should <span class=\"time-result\">set your alarm at "+date.getHours()+":"+date.getMinutes()+"</span>"
+    +"</br>to complete five 90-minute sleep cycles.";
+    // 6 cycles
+    
 }
